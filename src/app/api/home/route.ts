8d5a66a72c4491796db6ca3238a28100171ed4f3
alipy <ali.pyprogrammer@gmail.com>
2026-01-02 13:20:41 +0330

    update url

--- src/app/api/home/route.ts
+++ src/app/api/home/route.ts
@@ -1,10 +1,13 @@
 import { NextResponse } from "next/server";
 
-export const revalidate = 300;
-
 export async function GET() {
   try {
-    const res = await fetch("https://curtain.linooxel.com/api/ui/page/home");
+    const res = await fetch(
+      "http://curtain.linooxel.com:5042/api/ui/page/home",
+      {
+        next: { revalidate: 300 }
+      }
+    );
 
     if (!res.ok) {
       throw new Error(`API failed: ${res.status}`);
@@ -12,7 +15,6 @@ export async function GET() {
 
     const data = await res.json();
     return NextResponse.json(data);
-    
   } catch (error) {
     console.error('API Error:', error);
     return NextResponse.json(
